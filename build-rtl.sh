59cd6b343d6e4fdbb05aaccb2cea7546415bc4e7
Daniel Schloms <daniel.schloms@gmail.com>
2026-01-20 13:55:54 +0000

    Update scripts

--- build-rtl.sh
+++ build-rtl.sh
@@ -8,53 +8,52 @@ NC='\033[0m' # No Color
 
 # Defaults and setup
 ARCH="rv32im_zve32x"
-FLOAT_ARCH="rv32imf_zve32f"
 VLEN=64
 VLANE_W=32
 VMEM_W=32
+CLEAN_BEFORE="false"
+GENERATOR="Ninja"
 
 # Args
 TRACE="Off"
 TRACE_FULL="Off"
 
-# VLEN arg
-VLEN_NEXT="false"
-VLANE_NEXT="false"
-ARCH_NEXT="false"
-ARCH_SET="false"
-for arg in "$@"; do
-    if [ "$VLEN_NEXT" = "true" ]; then
-        VLEN="$arg"
-        VLEN_NEXT="false"
-    elif [ "$VLANE_NEXT" = "true" ]; then
-        VLANE_W="$arg"
-        VLANE_NEXT="false"
-    elif [ "$ARCH_NEXT" = "true" ]; then
-        ARCH="$arg"
-        ARCH_NEXT="false"
-        ARCH_SET="true"
-    elif [ "$arg" = "vlen" ]; then
-        VLEN_NEXT="true"
-    elif [ "$arg" = "vlane" ]; then
-        VLANE_NEXT="true"
-    elif [ "$arg" = "arch" ]; then
-        ARCH_NEXT="true"
-    elif [ "$arg" = "trace" ]; then
-        TRACE="On"
-        TRACE_FULL="On"
-    elif [ "$arg" = "float" ]; then
-        if [ "$ARCH_SET" = "true" ]; then
-            >&2 echo "Setting arch & float!"
-            exit 1
-        fi
-        ARCH=$FLOAT_ARCH
-    else
-        >&2 echo "Args: [trace, float, vlen <VLEN>, vlane <VLANE_W>, arch <ARCH>]"
-        exit 1
-    fi
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --arch)
+      ARCH="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --vlen)
+      VLEN="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --vlane_width)
+      VLANE_W="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --trace)
+      TRACE="On"
+      TRACE_FULL="On"
+      shift # past argument
+      ;;
+    --clean)
+      CLEAN_BEFORE="true"
+      shift # past argument
+      ;;
+    -*|--*)
+      >&2 echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
 done
-
-MODEL_FLAGS="-DVREG_W=$VLEN -DVLANE_W=$VLANE_W -DVMEM_W=$VMEM_W -DTRACE=$TRACE -DTRACE_FULL=$TRACE_FULL"
 
 # Paths
 PRJ_DIR=$WS_PATH/vicuna2_tinyml_benchmarking
@@ -64,15 +63,20 @@ MODEL_BUILD_DIR=$CMAKE_SRC_DIR/$ARCH/zvl${VLEN}b/vlane${VLANE_W}
 echo -e "${BLUE}Arch:${NC} $ARCH"
 echo -e "${BLUE}Model Flags:${NC} $MODEL_FLAGS"
 
-rm -rf $MODEL_BUILD_DIR
-
+if [[ "$CLEAN_BEFORE" = "true" ]]; then
+    rm -rf $MODEL_BUILD_DIR
+fi
 mkdir -p $MODEL_BUILD_DIR
 
-# Build Verilator model
-cd "$CMAKE_SRC_DIR" || exit
+cmake -S $CMAKE_SRC_DIR -B $MODEL_BUILD_DIR \
+    -DRISCV_ARCH=$ARCH \
+    -DVREG_W=$VLEN \
+    -DVLANE_W=$VLANE_W \
+    -DVMEM_W=$VMEM_W \
+    -DTRACE=$TRACE \
+    -DTRACE_FULL=$TRACE_FULL \
+    -G "$GENERATOR"
 
-cmake -S . -B $MODEL_BUILD_DIR -DRISCV_ARCH=$ARCH $MODEL_FLAGS
-cd "$MODEL_BUILD_DIR" || exit
+cmake --build $MODEL_BUILD_DIR -j$(nproc)
 
-make -j$(nproc)
 echo -e "${MAGENTA}Model done${NC}"
--- run-target.sh
+++ run-target.sh
@@ -6,7 +6,6 @@ BLUE='\033[0;34m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-TARGET_SW="bench"
 MEM_W=32
 VLEN=64
 VLANE_W=32
@@ -15,15 +14,44 @@ MEM_SIZE=805306368 # 0x30000000
 MEM_LATENCY=1
 EXTRA_CYCLES=1
 
-# Arguments
-if [ "$#" -ne 3 ]; then
-    echo "Usage: run-target.sh <target> <VLEN> <VLANE_W>"
-    exit 1
-fi
+ARCH="rv32im_zve32x"
+VLEN="64"
+VLANE_W="32"
+TARGET_SW=""
 
-TARGET_SW=$1
-VLEN=$2
-VLANE_W=$3
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --arch)
+      ARCH="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --vlen)
+      VLEN="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --vlane_width)
+      VLANE_W="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --target)
+      TARGET_SW="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      >&2 echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
 
 echo -e "Running ${BLUE}$TARGET_SW${NC}"
 
@@ -31,19 +59,28 @@ echo -e "Running ${BLUE}$TARGET_SW${NC}"
 
 # Verilator executable & args
 PRJ_DIR="$WS_PATH/vicuna2_tinyml_benchmarking"
-MODEL_BUILD_DIR="$PRJ_DIR/build_model/$ARCH/zvl{$VLEN}b/vlane${VLANE_W}"
-PROG_DIR="$PRJ_DIR/build_from_other/prog"
+ZVL_STRING="zvl${VLEN}b"
+VLANE_STRING="vlane${VLANE_W}"
+GCC_ARCH_SUBPATH="${ARCH}/${ZVL_STRING}"
+HW_ARCH_SUBPATH="${GCC_ARCH_SUBPATH}/${VLANE_STRING}"
+
+MODEL_BUILD_DIR="$PRJ_DIR/build_model/${HW_ARCH_SUBPATH}"
+PROG_DIR="$PRJ_DIR/build_from_other/${GCC_ARCH_SUBPATH}/prog"
 PROG_FILE="$PROG_DIR/prog_$TARGET_SW.txt"
-TRACE_COMP_DIR="$WS_PATH/vicuna2_tinyml_benchmarking/trace_comparison"
-TRACES_DIR="$TRACE_COMP_DIR/verilator"
-INSTR_TRACE_FILE="$TRACES_DIR/${TARGET_SW}_trace.txt"
-MEM_TRACE_FILE="$TRACES_DIR/${TARGET_SW}_mem_trace.csv"
-SIGNAL_TRACE_FILE="$TRACES_DIR/${TARGET_SW}_signal_trace.vcd"
+COMPARISON_DIR="$WS_PATH/rvv_testing/comparison"
+TRACE_DIR="$COMPARISON_DIR/verilator/${HW_ARCH_SUBPATH}"
+mkdir -p $TRACE_DIR
+
+INSTR_TRACE_FILE="$TRACE_DIR/${TARGET_SW}_trace.txt"
+# MEM_TRACE_FILE="$TRACE_DIR/${TARGET_SW}_mem_trace.csv"
+MEM_TRACE_FILE="/dev/null"
+SIGNAL_TRACE_FILE="$TRACE_DIR/${TARGET_SW}_signal_trace.vcd"
 
 VERILATOR_EXE="$MODEL_BUILD_DIR/verilated_model"
 VERILATOR_ARGS="$PROG_FILE $MEM_W $MEM_SIZE $MEM_LATENCY $EXTRA_CYCLES $INSTR_TRACE_FILE $MEM_TRACE_FILE $SIGNAL_TRACE_FILE"
+# VERILATOR_ARGS="$PROG_FILE $MEM_W $MEM_SIZE $MEM_LATENCY $EXTRA_CYCLES"
 VERILATOR_INVOKE="$VERILATOR_EXE $VERILATOR_ARGS"
 
-eval $VERILATOR_INVOKE
+time $VERILATOR_INVOKE
 echo -e "${MAGENTA}Done${NC}"
 
